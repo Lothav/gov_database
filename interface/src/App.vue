be00ee9dda11285789d707c146e013c0d132f29d
Luiz0tavio <me@luizotav.io>
2017-11-25 17:58:13 -0200

    text-p component

--- interface/src/App.vue
+++ interface/src/App.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="app">
-    <HelloWorld/>
+    <text-p v-for="desc in descs" :key="desc" :text="desc"/>
   </div>
 </template>
 <script>
-import HelloWorld from './components/HelloWorld'
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import TextP from './components/TextP'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        descs: [
+          'A base de dados de ocorrências aronáuticas é gerenciada pelo Centro de Investigação e Prevenção de Acidentes aeonáuticos (CENIPA). Constam nesta base de dados as ocorrências aeronáuticas notificadas ao CENIPA nos últimos 10 anos e que ocorreram em solo brasileiro.',
+          'Dentre as Informações disponíveis estão os dados sobre as aeronaves envolvidas, fatalidades, local, data e horário dos eventos e informações taxonômicas típicas das investigações de acidentes (AIG). São resguardadas a privacidade de pessoas físicas/jurídicas envolvidas conforme previsto pela Lei de Acesso à Informação.',
+          'Essa base de dados é composta por informações preliminares (provenientes do formulário CENIPA-05 [Ficha de Notificação de Ocorrência Aeronáutica]) e consolidadas (provenientes dos relatórios de investigações publicados).'
+        ]
+      }
+    },
+    components: {
+      TextP
+    }
   }
-}
 </script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ interface/src/components/TextP.vue
@@ -0,0 +1,36 @@
+<template>
+  <animated-fade-in>
+    <p>{{text}}</p>
+  </animated-fade-in>
+</template>
+<script>
+  export default {
+    name: 'text-p',
+    props: ['text'],
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  .description {
+    text-align: left;
+  }
+</style>
