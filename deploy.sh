e932e826260a74558fddaa8cb1cb08ede01e78c1
Luiz Otavio <me@luizotav.io>
2017-11-26 15:11:31 +0000

    for comment

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ sed -i -e 's/\/static/\/ibd\/static/g' ~/gov_database/interface/dist/index.html
 cd ~/gov_database/interface/dist/static/js &&
 for i in *; do
     sed -i -e 's/\/static/\/ibd\/static/g' $i
-done &&
+done
 #rm -r ~/LuizOtav.io/ibd &&
-mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
-cd ~/LuizOtav.io &&
-git commit -am "$1" &&
-git push
+#mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
+#cd ~/LuizOtav.io &&
+#git commit -am "$1" &&
+#git push
