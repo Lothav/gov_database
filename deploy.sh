2f7bf2b3a04e0c7eaadeb1db1dbe1fa5ebecf5b7
Luiz Otavio <me@luizotav.io>
2017-11-26 16:40:48 +0000

    add *

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ git commit -am "$1" &&
 git push &&
 cd ~/gov_database/interface &&
 npm run build &&
-
 rm -r ~/LuizOtav.io/ibd &&
 mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
 cd ~/LuizOtav.io &&
+git add * &&
 git commit -am "$1" &&
 git push
