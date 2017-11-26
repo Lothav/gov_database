b6aa80e71beae41822a5448d9471ace7e91b4d1c
Luiz Otavio <me@luizotav.io>
2017-11-26 15:05:39 +0000

    for javascript

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 git commit -am "$1" &&
 git push &&
-./interface/npm run build &&
+cd interface &&
+npm run build &&
+cd ../ &&
 sed -i -e 's/\/static/\/ibd\/static/g' ./interface/dist/index.html
 for i in ./interface/dist/js/*; do
     sed -i -e 's/\/static/\/ibd\/static/g' $i
