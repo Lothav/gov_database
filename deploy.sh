ea522267a5b6e02418a283a9ca45a186de482acc
Luiz Otavio <me@luizotav.io>
2017-11-26 15:08:50 +0000

    for javascript

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 git commit -am "$1" &&
 git push &&
-cd interface &&
+cd ~/gov_database/interface &&
 npm run build &&
-cd ../ &&
-sed -i -e 's/\/static/\/ibd\/static/g' ./interface/dist/index.html
-cd ./interface/dist/js &&
+sed -i -e 's/\/static/\/ibd\/static/g' ~/gov_database/interface/dist/index.html
+cd ~/gov_database/interface/dist/js &&
 for i in *; do
     sed -i -e 's/\/static/\/ibd\/static/g' $i
 done &&
 rm -r ~/LuizOtav.io/ibd &&
-mv ./interface/dist ~/LuizOtav.io/ibd &&
+mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
 cd ~/LuizOtav.io &&
 git commit -am "$1" &&
 git push
