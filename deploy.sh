497e8073876147a26e497ad93d9a065c6435f386
Luiz Otavio <me@luizotav.io>
2017-11-26 15:41:37 +0000

    ibd build subfolder

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,8 @@ git commit -am "$1" &&
 git push &&
 cd ~/gov_database/interface &&
 npm run build &&
-sed -i -e 's/\/static/\/ibd\/static/g' ~/gov_database/interface/dist/index.html
-cd ~/gov_database/interface/dist/static/js &&
-for i in *; do
-    sed -i -e 's/\/static/\/ibd\/static/g' $i
-done
-#rm -r ~/LuizOtav.io/ibd &&
-#mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
-#cd ~/LuizOtav.io &&
-#git commit -am "$1" &&
-#git push
+rm -r ~/LuizOtav.io/ibd &&
+mv ~/gov_database/interface/dist ~/LuizOtav.io/ibd &&
+cd ~/LuizOtav.io &&
+git commit -am "$1" &&
+git push
