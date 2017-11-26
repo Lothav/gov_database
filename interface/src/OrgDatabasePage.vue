9551438785d9392a460f94d5acdb3ff81465b711
Luiz Otavio <me@luizotav.io>
2017-11-26 14:41:02 +0000

    rows count

--- interface/src/OrgDatabasePage.vue
+++ interface/src/OrgDatabasePage.vue
@@ -8,8 +8,9 @@
     <div id="dataset" >
       <img src="./assets/data_model.png" alt="">
     </div>
-    <b-form-input v-model="query" type="text" placeholder="Insira a query SQL"></b-form-input>
-    <button @click="getQuery()">Get Query</button>
+    <b-form-input @keyup.enter.native="getQuery()" v-model="query" type="text" placeholder="Insira a query SQL"></b-form-input>
+    <h2 v-if="rowCount != -1">Total de resultados: {{rowCount}}</h2>
+    <b-table striped hover :items="rows"></b-table>
   </div>
 </template>
 <script>
@@ -19,6 +20,8 @@
     data () {
       return {
         query: '',
+        rows: [],
+        rowCount: -1,
         descs: [
           'A base de dados de ocorrências aronáuticas é gerenciada pelo Centro de Investigação e Prevenção de Acidentes aeonáuticos (CENIPA). Constam nesta base de dados as ocorrências aeronáuticas notificadas ao CENIPA nos últimos 10 anos e que ocorreram em solo brasileiro.',
           'Dentre as Informações disponíveis estão os dados sobre as aeronaves envolvidas, fatalidades, local, data e horário dos eventos e informações taxonômicas típicas das investigações de acidentes (AIG). São resguardadas a privacidade de pessoas físicas/jurídicas envolvidas conforme previsto pela Lei de Acesso à Informação.',
@@ -33,8 +36,11 @@
       getQuery: function () {
         let baseUrl = 'https://luizotavioapi.herokuapp.com/getQuery?q='
         this.$http.get(baseUrl + this.query).then(response => {
+          if (response.body && response.body.rows && response.body.rows.length) {
+            this.rows = response.body.rows
+            this.rowCount = response.body.rowCount
+          }
           console.log(response)
-          this.someData = response.body
         }, response => {
           console.log(response)
         })
